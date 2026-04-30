bd49191689bee40f5a45b033745cac2ad2fd717c
vwestberg <vwestberg@lselectric.com>
2026-04-30 11:26:58 -0500

    fix: use environment.systemPackages for wlr-randr, add fixhdmi alias and exec-once

--- users/vwestberg/applications/aliases.nix
+++ users/vwestberg/applications/aliases.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
-  home.packages = [ pkgs.wlr-randr ];
+  environment.systemPackages = [ pkgs.wlr-randr ];
 
   programs.zsh.shellAliases = {
     rebuild = "{ [ -d /tmp/eiros ] || gh repo clone 4thehalibit/eiros /tmp/eiros; } && sudo nixos-rebuild switch --impure --flake '/tmp/eiros#default' --override-input eiros_users '/home/vwestberg/eiros-config' --override-input eiros_hardware 'github:4thehalibit/eiros.hardware.vwestberg'";
