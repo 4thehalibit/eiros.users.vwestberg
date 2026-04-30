8790bad7415a46f18054e1fb7613631e445fdbfd
vwestberg <vwestberg@lselectric.com>
2026-04-30 15:44:16 -0500

    update config

--- users/vwestberg/dms.nix
+++ users/vwestberg/dms.nix
@@ -1,19 +1,4 @@
 { ... }:
 {
-  eiros.users.vwestberg.dms.clobber = false;
-
-  eiros.system.user_defaults.dms = {
-    theme = {
-      matugen_scheme = "scheme-rainbow";
-    };
-
-    appearance = {
-      widget_color_mode = "colorful";
-      bar_elevation.enable = false;
-    };
-
-    app_theming = {
-      terminals_always_dark = false;
-    };
-  };
+  eiros.users.vwestberg.dms.settings = builtins.fromJSON (builtins.readFile ../../dms-settings.json);
 }
--- users/vwestberg/user.nix
+++ users/vwestberg/user.nix
@@ -1,9 +1,7 @@
 { ... }:
 {
   config = {
-    eiros.users.vwestberg = {
-      dms.settings.use24HourClock = false;
-    };
+    eiros.users.vwestberg = {};
     # Generate your password hash with: mkpasswd -m yescrypt
     # Then replace the value below with your hash.
     users.users.vwestberg.initialHashedPassword = "REPLACE_WITH_HASH";
