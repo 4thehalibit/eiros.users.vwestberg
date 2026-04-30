8f4c66b6c405cee7580b3f2a09c4bfd6b2d2cb82
vwestberg <vwestberg@lselectric.com>
2026-04-30 10:31:38 -0500

    update config

--- users/vwestberg/applications/aliases.nix
+++ users/vwestberg/applications/aliases.nix
@@ -3,6 +3,5 @@
   programs.zsh.shellAliases = {
     rebuild = "sudo nixos-rebuild switch --flake '/tmp/eiros#default' --override-input eiros_users '/home/vwestberg/eiros-config' --override-input eiros_hardware 'github:4thehalibit/eiros.hardware.vwestberg'";
     deploy = "cd ~/eiros-config && git add -A && git commit -m 'update config' && git push && rebuild";
-    update-ninja = "{ deb=\$(ls ~/Downloads/ninjarmm-ncplayer-*.deb 2>/dev/null | sort -V | tail -1) && [ -n \"\$deb\" ] && nix run nixpkgs#dpkg -- -x \"\$deb\" /home/vwestberg/.local/opt/ncplayer && echo \"Updated from \$deb\" || echo 'No ninjarmm-ncplayer*.deb found in ~/Downloads'; }";
   };
 }
---
+++ users/vwestberg/applications/ninjaone_config.nix
@@ -0,0 +1,8 @@
+{ ... }:
+{
+  eiros.system.default_applications.utilities.ninjaone = {
+    enable = true;
+    deb_path = /home/vwestberg/private/ninjarmm-ncplayer_amd64.deb;
+    update_alias.enable = true;
+  };
+}
