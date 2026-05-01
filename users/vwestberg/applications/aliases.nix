ccbdbaa3c0c3f9274fe16b79efe858b44d7bf462
vwestberg <vwestberg@lselectric.com>
2026-05-01 15:39:58 -0500

    update config

--- users/vwestberg/applications/aliases.nix
+++ users/vwestberg/applications/aliases.nix
@@ -3,7 +3,7 @@
   environment.systemPackages = [ pkgs.wlr-randr ];
 
   programs.zsh.shellAliases = {
-    rebuild = "{ [ -d /tmp/eiros ] || gh repo clone lcleveland/eiros /tmp/eiros; cd /tmp/eiros && git pull; } && sudo nixos-rebuild switch --impure --flake '/tmp/eiros#default' --override-input eiros_users '/home/vwestberg/eiros-config' --override-input eiros_hardware 'github:4thehalibit/eiros.hardware.vwestberg'";
+    rebuild = "{ [ -d /tmp/eiros ] || gh repo clone lcleveland/eiros /tmp/eiros; cd /tmp/eiros && git pull; } && sudo nixos-rebuild switch --impure --flake '/tmp/eiros#default' --override-input eiros_users '/home/vwestberg/eiros-config' --override-input eiros_hardware 'github:4thehalibit/eiros.hardware.framework16'";
     deploy = "cd ~/eiros-config && git add -A && git commit -m 'update config' && git push && rebuild";
     save-dms = "cp ~/.config/DankMaterialShell/settings.json ~/eiros-config/dms-settings.json && echo 'DMS settings saved'";
     fixhdmi = "output=$(wlr-randr | grep 'VX3211-4K' | awk '{print $1}') && [ -n \"$output\" ] && wlr-randr --output \"$output\" --off && sleep 2 && wlr-randr --output \"$output\" --on || echo 'ViewSonic not detected'";
--- users/vwestberg/sources.nix
+++ users/vwestberg/sources.nix
@@ -1,8 +1,8 @@
 { ... }:
 {
   eiros.system.nix.sources = {
-    users.url = "github:4thehalibit/eiros.users.vwestberg";
-    hardware.url = "github:4thehalibit/eiros.hardware.vwestberg";
+    users.url = "github:4thehalibit/eiros.users.personal";
+    hardware.url = "github:4thehalibit/eiros.hardware.framework16";
   };
 
   environment.variables.NH_FLAKE = "github:lcleveland/eiros";
