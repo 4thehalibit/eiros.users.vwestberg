dc6a74d894610c45c0170b55e6376d5559dc0020
vwestberg <vwestberg@lselectric.com>
2026-05-04 14:33:19 -0500

    patch waydroid to use iptables-nft instead of iptables-legacy

--- users/vwestberg/applications/gaming.nix
+++ users/vwestberg/applications/gaming.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   programs.steam = {
     enable = true;
@@ -8,4 +8,18 @@
   };
 
   virtualisation.waydroid.enable = true;
+
+  # Patch waydroid to skip iptables-legacy (missing kernel module on this kernel)
+  # and fall back to iptables-nft which works via the loaded nf_tables/nf_nat modules.
+  nixpkgs.overlays = [
+    (final: prev: {
+      waydroid = prev.waydroid.overrideAttrs (old: {
+        postInstall = (old.postInstall or "") + ''
+          substituteInPlace $out/lib/waydroid/data/scripts/.waydroid-net.sh-wrapped \
+            --replace 'IPTABLES_BIN="$(command -v iptables-legacy)"' 'IPTABLES_BIN=""' \
+            --replace 'IP6TABLES_BIN="$(command -v ip6tables-legacy)"' 'IP6TABLES_BIN=""'
+        '';
+      });
+    })
+  ];
 }
